e6154426d4b80632e73cc8f93d1498f3743db265
Dale McConachie <dmcconachie@wpi.edu>
2015-11-11 21:56:48 -0500

    Added a wrapper for nh.getParam in ros_helpers.hpp

---
+++ include/arc_utilities/ros_helpers.hpp
@@ -0,0 +1,41 @@
+#include <ros/ros.h>
+
+#ifndef ROS_HELPERS_HPP
+#define ROS_HELPERS_HPP
+
+namespace ROSHelpers
+{
+    template <typename T>
+    inline T GetParam(ros::NodeHandle& nh, const std::string& param_name, const T& default_val)
+    {
+        T param_val;
+        if (nh.getParam(param_name, param_val))
+        {
+            ROS_INFO_STREAM("Setting " << param_name << " to " << param_val);
+        }
+        else
+        {
+            param_val = default_val;
+            ROS_WARN_STREAM(param_name << " not set! Using default of " << param_val);
+        }
+        return param_val;
+    }
+
+    template <typename T>
+    inline T GetParam(ros::NodeHandle& nh, const std::string& param_name, T&& default_val)
+    {
+        T param_val;
+        if (nh.getParam(param_name, param_val))
+        {
+            ROS_INFO_STREAM("Setting " << param_name << " to " << param_val);
+        }
+        else
+        {
+            param_val = default_val;
+            ROS_WARN_STREAM(param_name << " not set! Using default of " << param_val);
+        }
+        return param_val;
+    }
+}
+
+#endif // ROS_HELPERS_HPP
