8bc9999bfbda0b0ec3c818bd3fb693a497f4ed03
Dale McConachie <dmcconac@umich.edu>
2017-06-08 15:58:57 -0400

    Revised ROSHelpers output format to be easier to read.

--- include/arc_utilities/ros_helpers.hpp
+++ include/arc_utilities/ros_helpers.hpp
@@ -22,12 +22,12 @@ namespace ROSHelpers
         T param_val;
         if (nh.getParam(param_name, param_val))
         {
-            ROS_INFO_STREAM("Setting " << param_name << " to " << param_val);
+            ROS_INFO_STREAM_NAMED("params", "Setting    " << std::left << std::setw(40) << param_name << " to " << param_val);
         }
         else
         {
             param_val = default_val;
-            ROS_WARN_STREAM(param_name << " not set! Using default of " << param_val);
+            ROS_WARN_STREAM_NAMED("params", "Defaulting " << std::left << std::setw(40) << param_name << " to " << param_val);
         }
         return param_val;
     }
@@ -38,12 +38,12 @@ namespace ROSHelpers
         T param_val;
         if (nh.getParam(param_name, param_val))
         {
-            ROS_INFO_STREAM("Setting " << param_name << " to " << param_val);
+            ROS_INFO_STREAM_NAMED("params", "Setting    " << std::left << std::setw(40) << param_name << " to " << param_val);
         }
         else
         {
             param_val = default_val;
-            ROS_WARN_STREAM(param_name << " not set! Using default of " << param_val);
+            ROS_WARN_STREAM_NAMED("params", "Defaulting " << std::left << std::setw(40) << param_name << " to " << param_val);
         }
         return param_val;
     }
@@ -55,12 +55,12 @@ namespace ROSHelpers
         T param_val;
         if (nh.getParam(param_name, param_val))
         {
-            ROS_INFO_STREAM("Setting " << param_name << " to " << param_val);
+            ROS_INFO_STREAM_NAMED("params", "Setting    " << std::left << std::setw(40) << param_name << " to " << param_val);
         }
         else
         {
             param_val = default_val;
-            ROS_INFO_STREAM(param_name << " not set! Using default of " << param_val);
+            ROS_INFO_STREAM_NAMED("params", "Defaulting " << std::left << std::setw(40) << param_name << " to " << param_val);
         }
         return param_val;
     }
@@ -71,12 +71,12 @@ namespace ROSHelpers
         T param_val;
         if (nh.getParam(param_name, param_val))
         {
-            ROS_INFO_STREAM("Setting " << param_name << " to " << param_val);
+            ROS_INFO_STREAM_NAMED("params", "Setting    " << std::left << std::setw(40) << param_name << " to " << param_val);
         }
         else
         {
             param_val = default_val;
-            ROS_INFO_STREAM(param_name << " not set! Using default of " << param_val);
+            ROS_INFO_STREAM_NAMED("params", "Defaulting " << std::left << std::setw(40) << param_name << " to " << param_val);
         }
         return param_val;
     }
@@ -84,16 +84,16 @@ namespace ROSHelpers
     template <typename T>
     inline Maybe::Maybe<T> GetParamRequired(ros::NodeHandle& nh, const std::string& param_name, const std::string& calling_fn_name)
     {
-        ROS_DEBUG_STREAM("No default value for " << param_name << ": Value must be on paramter sever");
+        ROS_DEBUG_STREAM_NAMED("params", "No default value for " << param_name << ": Value must be on paramter sever");
         T param_val;
         if (nh.getParam(param_name, param_val))
         {
-            ROS_INFO_STREAM("Setting " << param_name << " to " << param_val);
+            ROS_INFO_STREAM_NAMED("params", "Setting    " << std::left << std::setw(40) << param_name << " to " << param_val);
             return Maybe::Maybe<T>(param_val);
         }
         else
         {
-            ROS_FATAL_STREAM("Cannot find " << param_name << " on parameter server for " << calling_fn_name << ": Value must be on paramter sever");
+            ROS_FATAL_STREAM_NAMED("params", "Cannot find " << param_name << " on parameter server for " << calling_fn_name << ": Value must be on paramter sever");
             return Maybe::Maybe<T>();
         }
     }
