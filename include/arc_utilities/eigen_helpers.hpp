26858df949117e1afc0f384267983c9c75877aee
Dale McConachie <dmcconac@umich.edu>
2018-09-05 13:54:06 -0400

    Re-ordered functions for better flow of file.

--- include/arc_utilities/eigen_helpers.hpp
+++ include/arc_utilities/eigen_helpers.hpp
@@ -286,150 +286,6 @@ namespace EigenHelpers
         return std::sqrt(SquaredNorm(vec));
     }
 
-    template <typename T>
-    inline std::vector<T> Abs(const std::vector<T>& vec)
-    {
-        std::vector<T> absed(vec.size(), 0.0);
-        for (size_t idx = 0; idx < absed.size(); idx++)
-        {
-            absed[idx] = std::abs(vec[idx]);
-        }
-        return absed;
-    }
-
-    template <typename T1, typename T2>
-    inline std::vector<T1> Multiply(const std::vector<T1>& vec, const T2 scalar)
-    {
-        std::vector<T1> multiplied(vec.size(), 0.0);
-        for (size_t idx = 0; idx < multiplied.size(); idx++)
-        {
-            const double element = vec[idx];
-            multiplied[idx] = element * scalar;
-        }
-        return multiplied;
-    }
-
-    template <typename T>
-    inline std::vector<T> Multiply(const std::vector<T>& vec1, const std::vector<T>& vec2)
-    {
-        if (vec1.size() == vec2.size())
-        {
-            std::vector<T> multiplied(vec1.size(), 0.0);
-            for (size_t idx = 0; idx < multiplied.size(); idx++)
-            {
-                const double element1 = vec1[idx];
-                const double element2 = vec2[idx];
-                multiplied[idx] = element1 * element2;
-            }
-            return multiplied;
-        }
-        else
-        {
-            return std::vector<T>();
-        }
-    }
-
-    template <typename T1, typename T2>
-    inline std::vector<T1> Divide(const std::vector<T1>& vec, const T2 scalar)
-    {
-        const double inv_scalar = 1.0 / scalar;
-        return Multiply(vec, inv_scalar);
-    }
-
-    template <typename T>
-    inline std::vector<T> Divide(const std::vector<T>& vec1, const std::vector<T>& vec2)
-    {
-        if (vec1.size() == vec2.size())
-        {
-            std::vector<T> divided(vec1.size(), 0.0);
-            for (size_t idx = 0; idx < divided.size(); idx++)
-            {
-                const T element1 = vec1[idx];
-                const T element2 = vec2[idx];
-                divided[idx] = element1 / element2;
-            }
-            return divided;
-        }
-        else
-        {
-            return std::vector<T>();
-        }
-    }
-
-    template <typename T1, typename T2>
-    inline std::vector<T1> Add(const std::vector<T1>& vec, const T2 scalar)
-    {
-        std::vector<T1> added(vec.size(), 0.0);
-        for (size_t idx = 0; idx < added.size(); idx++)
-        {
-            added[idx] = vec[idx] + scalar;
-        }
-        return added;
-    }
-
-    template <typename T>
-    inline std::vector<double> Add(const std::vector<T>& vec1, const std::vector<T>& vec2)
-    {
-        if (vec1.size() == vec2.size())
-        {
-            std::vector<T> added(vec1.size(), 0.0);
-            for (size_t idx = 0; idx < added.size(); idx++)
-            {
-                const T element1 = vec1[idx];
-                const T element2 = vec2[idx];
-                added[idx] = element1 + element2;
-            }
-            return added;
-        }
-        else
-        {
-            return std::vector<T>();
-        }
-    }
-
-    template <typename T1, typename T2>
-    inline std::vector<T1> Sub(const std::vector<T1>& vec, const T2 scalar)
-    {
-        std::vector<T1> subed(vec.size(), 0.0);
-        for (size_t idx = 0; idx < subed.size(); idx++)
-        {
-            subed[idx] = vec[idx] - scalar;
-        }
-        return subed;
-    }
-
-    template <typename T>
-    inline std::vector<T> Sub(const std::vector<T>& vec1, const std::vector<T>& vec2)
-    {
-        if (vec1.size() == vec2.size())
-        {
-            std::vector<T> subed(vec1.size(), 0.0);
-            for (size_t idx = 0; idx < subed.size(); idx++)
-            {
-                const T element1 = vec1[idx];
-                const T element2 = vec2[idx];
-                subed[idx] = element1 - element2;
-            }
-            return subed;
-        }
-        else
-        {
-            return std::vector<T>();
-        }
-    }
-
-    template <typename T>
-    inline T Sum(const std::vector<T>& vec)
-    {
-        T sum = 0.0;
-        for (size_t idx = 0; idx < vec.size(); idx++)
-        {
-            const T element = vec[idx];
-            sum += element;
-        }
-        return sum;
-    }
-
     inline Eigen::Matrix3d Skew(const Eigen::Vector3d& vector)
     {
         Eigen::Matrix3d skewed;
@@ -1053,7 +909,6 @@ namespace EigenHelpers
         return min_ind;
     }
 
-
     ////////////////////////////////////////////////////////////////////////////
     // Conversion functions
     ////////////////////////////////////////////////////////////////////////////
@@ -1241,6 +1096,154 @@ namespace EigenHelpers
     inline std::vector<double> EigenQuaterniondToStdVectorDouble(const Eigen::Quaterniond& quat)
     {
         return std::vector<double>{quat.x(), quat.y(), quat.z(), quat.w()};
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Math functions on std::vector types
+    ////////////////////////////////////////////////////////////////////////////
+
+    template <typename T>
+    inline std::vector<T> Abs(const std::vector<T>& vec)
+    {
+        std::vector<T> absed(vec.size(), 0.0);
+        for (size_t idx = 0; idx < absed.size(); idx++)
+        {
+            absed[idx] = std::abs(vec[idx]);
+        }
+        return absed;
+    }
+
+    template <typename T1, typename T2>
+    inline std::vector<T1> Multiply(const std::vector<T1>& vec, const T2 scalar)
+    {
+        std::vector<T1> multiplied(vec.size(), 0.0);
+        for (size_t idx = 0; idx < multiplied.size(); idx++)
+        {
+            const double element = vec[idx];
+            multiplied[idx] = element * scalar;
+        }
+        return multiplied;
+    }
+
+    template <typename T>
+    inline std::vector<T> Multiply(const std::vector<T>& vec1, const std::vector<T>& vec2)
+    {
+        if (vec1.size() == vec2.size())
+        {
+            std::vector<T> multiplied(vec1.size(), 0.0);
+            for (size_t idx = 0; idx < multiplied.size(); idx++)
+            {
+                const double element1 = vec1[idx];
+                const double element2 = vec2[idx];
+                multiplied[idx] = element1 * element2;
+            }
+            return multiplied;
+        }
+        else
+        {
+            return std::vector<T>();
+        }
+    }
+
+    template <typename T1, typename T2>
+    inline std::vector<T1> Divide(const std::vector<T1>& vec, const T2 scalar)
+    {
+        const double inv_scalar = 1.0 / scalar;
+        return Multiply(vec, inv_scalar);
+    }
+
+    template <typename T>
+    inline std::vector<T> Divide(const std::vector<T>& vec1, const std::vector<T>& vec2)
+    {
+        if (vec1.size() == vec2.size())
+        {
+            std::vector<T> divided(vec1.size(), 0.0);
+            for (size_t idx = 0; idx < divided.size(); idx++)
+            {
+                const T element1 = vec1[idx];
+                const T element2 = vec2[idx];
+                divided[idx] = element1 / element2;
+            }
+            return divided;
+        }
+        else
+        {
+            return std::vector<T>();
+        }
+    }
+
+    template <typename T1, typename T2>
+    inline std::vector<T1> Add(const std::vector<T1>& vec, const T2 scalar)
+    {
+        std::vector<T1> added(vec.size(), 0.0);
+        for (size_t idx = 0; idx < added.size(); idx++)
+        {
+            added[idx] = vec[idx] + scalar;
+        }
+        return added;
+    }
+
+    template <typename T>
+    inline std::vector<double> Add(const std::vector<T>& vec1, const std::vector<T>& vec2)
+    {
+        if (vec1.size() == vec2.size())
+        {
+            std::vector<T> added(vec1.size(), 0.0);
+            for (size_t idx = 0; idx < added.size(); idx++)
+            {
+                const T element1 = vec1[idx];
+                const T element2 = vec2[idx];
+                added[idx] = element1 + element2;
+            }
+            return added;
+        }
+        else
+        {
+            return std::vector<T>();
+        }
+    }
+
+    template <typename T1, typename T2>
+    inline std::vector<T1> Sub(const std::vector<T1>& vec, const T2 scalar)
+    {
+        std::vector<T1> subed(vec.size(), 0.0);
+        for (size_t idx = 0; idx < subed.size(); idx++)
+        {
+            subed[idx] = vec[idx] - scalar;
+        }
+        return subed;
+    }
+
+    template <typename T>
+    inline std::vector<T> Sub(const std::vector<T>& vec1, const std::vector<T>& vec2)
+    {
+        if (vec1.size() == vec2.size())
+        {
+            std::vector<T> subed(vec1.size(), 0.0);
+            for (size_t idx = 0; idx < subed.size(); idx++)
+            {
+                const T element1 = vec1[idx];
+                const T element2 = vec2[idx];
+                subed[idx] = element1 - element2;
+            }
+            return subed;
+        }
+        else
+        {
+            return std::vector<T>();
+        }
+    }
+
+    template <typename T>
+    inline T Sum(const std::vector<T>& vec)
+    {
+        T sum = 0.0;
+        for (size_t idx = 0; idx < vec.size(); idx++)
+        {
+            const T element = vec[idx];
+            sum += element;
+        }
+        return sum;
     }
 
     ////////////////////////////////////////////////////////////////////////////
