ea189829266cbf47be715655c8bb0db1c81649cd
Andrew Price <arprice@gatech.edu>
2019-04-03 17:40:52 -0400

    Fixed catkin-make-args in ci test.

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,4 +4,4 @@ BUILD_TYPE=${1:-Release}
 . /opt/ros/kinetic/setup.bash
 
 cd ~/catkin_ws
-catkin_make_isolated --catkin-make-args -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+catkin_make_isolated -DCMAKE_BUILD_TYPE=$BUILD_TYPE
--- docker/test.sh
+++ docker/test.sh
@@ -5,4 +5,4 @@ BUILD_TYPE=${1:-Release}
 . /root/catkin_ws/devel_isolated/setup.bash
 
 cd ~/catkin_ws
-catkin_make_isolated --catkin-make-args -DCMAKE_BUILD_TYPE=$BUILD_TYPE run_tests && catkin_test_results
+catkin_make_isolated -DCMAKE_BUILD_TYPE=$BUILD_TYPE --catkin-make-args run_tests && catkin_test_results
