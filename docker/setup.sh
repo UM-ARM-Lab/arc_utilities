bdffcfa630ea4a061ef194b70403f8b82459b3a0
Andrew Price <arprice@gatech.edu>
2019-04-03 15:02:48 -0400

    Testing out Travix build matrix for debug and release modes.

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,6 +1,7 @@
 #/bin/bash
+BUILD_TYPE=${1:-Release}
 
 . /opt/ros/kinetic/setup.bash
 
 cd ~/catkin_ws
-catkin_make_isolated
+catkin_make_isolated --catkin-make-args -DCMAKE_BUILD_TYPE=$BUILD_TYPE
--- docker/test.sh
+++ docker/test.sh
@@ -1,7 +1,8 @@
 #/bin/bash
+BUILD_TYPE=${1:-Release}
 
 . /opt/ros/kinetic/setup.bash
 . /root/catkin_ws/devel_isolated/setup.bash
 
 cd ~/catkin_ws
-catkin_make_isolated --catkin-make-args run_tests && catkin_test_results
+catkin_make_isolated --catkin-make-args -DCMAKE_BUILD_TYPE=$BUILD_TYPE run_tests && catkin_test_results
