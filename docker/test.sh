0909efae77c437bff2519bb5c7b6a5e2800beeee
Andrew Price <arprice@gatech.edu>
2019-04-04 00:06:08 -0400

    Travis Build Matrix (#76)
    * Added Travis build matrix for debug and release modes.
    
    * Added ROS versions to test matrix.
    
    * Print ROS version in Travis build step.

--- docker/test.sh
+++ docker/test.sh
@@ -1,7 +1,9 @@
 #/bin/bash
+ROS_VERSION=${1:-kinetic}
+BUILD_TYPE=${2:-Release}
 
-. /opt/ros/kinetic/setup.bash
+. /opt/ros/$ROS_VERSION/setup.bash
 . /root/catkin_ws/devel_isolated/setup.bash
 
 cd ~/catkin_ws
-catkin_make_isolated --catkin-make-args run_tests && catkin_test_results
+catkin_make_isolated -DCMAKE_BUILD_TYPE=$BUILD_TYPE --catkin-make-args run_tests && catkin_test_results
