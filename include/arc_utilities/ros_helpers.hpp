02dc9456275cbed6255784ff4b54c8b64e5e00ba
Dale McConachie <dmcconac@umich.edu>
2019-07-25 19:55:07 -0400

    Added rosparam vector retriever.

--- include/arc_utilities/ros_helpers.hpp
+++ include/arc_utilities/ros_helpers.hpp
@@ -115,6 +115,23 @@ namespace ROSHelpers
             return Maybe::Maybe<T>();
         }
     }
+
+    template <typename T>
+    inline Maybe::Maybe<std::vector<T>> GetVectorRequired(const ros::NodeHandle& nh, const std::string& param_name, const std::string& calling_fn_name)
+    {
+        ROS_DEBUG_STREAM_NAMED("params", "No default value for " << param_name << ": Value must be on paramter sever");
+        std::vector<T> vec;
+        if (nh.getParam(param_name, vec))
+        {
+            ROS_INFO_STREAM_NAMED("params", "Retrieving " << std::left << std::setw(PARAM_NAME_WIDTH) << param_name << " of length " << vec.size());
+            return Maybe::Maybe<std::vector<T>>(vec);
+        }
+        else
+        {
+            ROS_FATAL_STREAM_NAMED("params", "Cannot find " << nh.getNamespace() << "/" << param_name << " on parameter server for " << calling_fn_name << ": Value must be on paramter sever");
+            return Maybe::Maybe<std::vector<T>>();
+        }
+    }
 }
 
 #endif // ROS_HELPERS_HPP
