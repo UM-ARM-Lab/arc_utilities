c8868dcd98ac395eac0037e29e499760ae9ff161
Dale McConachie <dmcconac@umich.edu>
2019-08-15 00:04:22 -0400

    Added optional variants that output to debug if no param is found.

--- include/arc_utilities/ros_helpers.hpp
+++ include/arc_utilities/ros_helpers.hpp
@@ -83,6 +83,23 @@ namespace ROSHelpers
     }
 
     template <typename T>
+    inline Maybe::Maybe<T> GetParamOptional(const ros::NodeHandle& nh, const std::string& param_name, const std::string& calling_fn_name)
+    {
+        ROS_DEBUG_STREAM_NAMED("params", "No default value for " << param_name << ": Value must be on paramter sever");
+        T param_val;
+        if (nh.getParam(param_name, param_val))
+        {
+            ROS_INFO_STREAM_NAMED("params", "Retrieving " << std::left << std::setw(PARAM_NAME_WIDTH) << param_name << " as " << param_val);
+            return Maybe::Maybe<T>(param_val);
+        }
+        else
+        {
+            ROS_DEBUG_STREAM_NAMED("params", "Cannot find " << nh.getNamespace() << "/" << param_name << " on parameter server for " << calling_fn_name << ": Value must be on paramter sever");
+            return Maybe::Maybe<T>();
+        }
+    }
+
+    template <typename T>
     inline Maybe::Maybe<T> GetParamRequired(const ros::NodeHandle& nh, const std::string& param_name, const std::string& calling_fn_name)
     {
         ROS_DEBUG_STREAM_NAMED("params", "No default value for " << param_name << ": Value must be on paramter sever");
@@ -113,6 +130,23 @@ namespace ROSHelpers
         {
             ROS_FATAL_STREAM_NAMED("params", "Cannot find " << nh.getNamespace() << "/" << param_name << " on parameter server for " << calling_fn_name << ": Value must be on paramter sever");
             return Maybe::Maybe<T>();
+        }
+    }
+
+    template <typename T>
+    inline Maybe::Maybe<std::vector<T>> GetVectorOptional(const ros::NodeHandle& nh, const std::string& param_name, const std::string& calling_fn_name)
+    {
+        ROS_DEBUG_STREAM_NAMED("params", "No default value for " << param_name << ": Value must be on paramter sever");
+        std::vector<T> vec;
+        if (nh.getParam(param_name, vec))
+        {
+            ROS_INFO_STREAM_NAMED("params", "Retrieving " << std::left << std::setw(PARAM_NAME_WIDTH) << param_name << " of length " << vec.size());
+            return Maybe::Maybe<std::vector<T>>(vec);
+        }
+        else
+        {
+            ROS_INFO_STREAM_NAMED("params", "Cannot find " << nh.getNamespace() << "/" << param_name << " on parameter server for " << calling_fn_name << ": Value must be on paramter sever");
+            return Maybe::Maybe<std::vector<T>>();
         }
     }
 
