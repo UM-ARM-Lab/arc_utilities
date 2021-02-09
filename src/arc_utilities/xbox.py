68240c830b9b049cc5ae7254c233b631e9ce9026
Peter Mitrano <pmitrano@armstorm>
2021-02-09 10:25:54 -0500

    super hacky logitech controller class

--- src/arc_utilities/xbox.py
+++ src/arc_utilities/xbox.py
@@ -1,3 +1,6 @@
+import numpy as np
+from inputs import InputEvent
+
 import rospy
 from arc_utilities.listener import Listener
 from arc_utilities.ros_helpers import joy_to_xbox, wait_for
@@ -41,3 +44,40 @@ class Xbox():
             rospy.loginfo("Waiting for xbox button: " + button)
 
         wait_for(lambda: not self.get_button(button) == 0)
+
+
+class Logitech:
+
+    def __init__(self, joystick_topic="joy"):
+        self.xbox_listener = Listener(joystick_topic, Joy)
+
+    def x_clicked(self, event: InputEvent):
+        return event.ev_type == 'Key' and event.code == 'BTN_TRIGGER' and event.state == 0
+
+    def get_axis_normalized(self, axis: int):
+        joy_msg = self.xbox_listener.get()
+        if axis == 0:
+            return -joy_msg.axes[axis]
+        elif axis == 1:
+            return joy_msg.axes[axis]
+        elif axis == 4:
+            return -joy_msg.axes[axis]
+        elif axis == 5:
+            return joy_msg.axes[axis]
+        else:
+            raise NotImplementedError(f"axis {axis} is not implemented")
+
+    def get_3d_delta(self):
+        """
+        We use the dpad for x,y and the left joystick to do z
+
+        Args:
+            joy_msg:
+
+        Returns:
+            the [x, y, z], in the interval [-1.0, 1.0]
+
+        """
+        return np.array([self.get_axis_normalized(4),
+                         self.get_axis_normalized(5),
+                         self.get_axis_normalized(1)])
