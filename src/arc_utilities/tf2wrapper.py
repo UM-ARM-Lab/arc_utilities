2c5ceae7b1ad135e30ab4d3390b0a070b6595d3d
Peter <peter@armnova>
2021-12-08 10:50:58 -0500

    reduce timeout and import msgs so users don't get confusing error

--- src/arc_utilities/tf2wrapper.py
+++ src/arc_utilities/tf2wrapper.py
@@ -5,6 +5,8 @@ import rospy
 import tf2_ros
 from arc_utilities import transformation_helper
 from geometry_msgs.msg import Pose
+# noinspection PyUnresolvedReferences
+import tf2_geometry_msgs
 
 
 class TF2Wrapper:
@@ -51,7 +53,7 @@ class TF2Wrapper:
                           parent,
                           child,
                           verbose=True,
-                          spin_delay=rospy.Duration(secs=0, nsecs=500 * 1000 * 1000),
+                          spin_delay=rospy.Duration(secs=0, nsecs=5 * 1000 * 1000),
                           time=rospy.Time()):
         while not self.tf_buffer.can_transform(target_frame=parent, source_frame=child,
                                                time=time, timeout=spin_delay):
