bce3b74fca50b3bae1f8973e8b53ee2d7080b74e
Peter <peter@thinkpad_t15>
2024-01-18 13:45:29 -0500

    add back wait_for

--- arm_utilities/ros_helpers.py
+++ arm_utilities/ros_helpers.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python
+import time
+from typing import Optional
+
+import rclpy
+import rclpy.logging
 from sensor_msgs.msg import Joy
+
+logger = rclpy.logging.get_logger("ros_helpers")
 
 
 def joy_to_xbox(joy: Joy, xpad: bool = True):
@@ -27,3 +34,21 @@ def joy_to_xbox(joy: Joy, xpad: bool = True):
         x.A, x.B, x.X, x.Y, x.LB, x.RB, x.back, x.start, x.power, x.stick_button_left, x.stick_button_right = joy.buttons
         x.LH, x.LV, x.LT, x.RH, x.RV, x.RT, x.DH, x.DV = joy.axes
     return x
+
+
+def wait_for(func, warn_after: Optional[int] = 10, name: Optional[str] = ""):
+    """
+    Waits for function evaluation to be true. Exits cleanly from ros.
+
+    Introduces sleep delay, not recommended for time critical operations
+    """
+
+    start_t = time.time()
+
+    while not func() and not rclpy.ok():
+        if warn_after is not None and time.time() - start_t > warn_after:
+            warning = f"still waiting after {warn_after}s"
+            if name:
+                warning += f" for {name}"
+            logger.warn(warning, throttle_duration_sec=5.0)
+        time.sleep(0.01)
